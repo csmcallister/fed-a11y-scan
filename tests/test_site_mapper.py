a772130a307f471e6c4a125ffd0d0f40acdd4194
csmcallister <mcallistercs@gmail.com>
2020-07-02 16:40:45 -0400

    TST: format_url test :white_check_mark:

--- tests/test_site_mapper.py
+++ tests/test_site_mapper.py
@@ -1,5 +1,6 @@
 import boto3
 
+import pytest
 from tests.fixtures import *
 
 
@@ -23,3 +24,20 @@ def test_send_message(sqs):
     
     r = send_message({"Id": "1", "MessageBody": "foo"})
     assert "Successful" in r.keys()
+
+
+@pytest.mark.parametrize("url, expected", [
+    ('https://www.foo.gov', ('foo', '', '')),
+    ('https://www.bar.foo.gov', ('foo', 'bar', '')),
+    ('https://www.fiz.bar.foo.gov', ('foo', 'fiz.bar', '')),
+    ('https://www.foo.gov/baz', ('foo', '', 'baz')),
+    ('https://www.foo.gov/baz/biz', ('foo', '', 'baz/biz') ),
+    ('https://www.bar.foo.gov/baz/biz', ('foo', 'bar', 'baz/biz') ),
+    ('https://www.fiz.foo.gov/bar', ('foo', 'fiz', 'bar')),
+    ('https://foo.gov', ('foo', '', '')),
+    ('https://bar.foo.gov', ('foo', 'bar', ''))
+])
+def test_format_url(url, expected):
+    from lambdas.site_mapper.handler import MySpider
+    result = MySpider.format_url(url)
+    assert result == expected
