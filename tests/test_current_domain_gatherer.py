e48b09ee41ba43e8c4ec11335bb9a5a1e0765ea1
csmcallister <mcallistercs@gmail.com>
2020-07-01 21:59:38 -0400

    TST: add unit tests for lambdas :white_check_mark:

---
+++ tests/test_current_domain_gatherer.py
@@ -0,0 +1,38 @@
+import json
+
+from tests.fixtures import *
+
+
+def test_main(sqs, s3):
+    from lambdas.current_domain_gatherer import handler
+    
+    BUCKET = os.environ['BUCKET_NAME']
+    OBJ_KEY = os.environ['OBJECT_KEY']
+    SQS_URL = os.environ['SQS_URL']
+    s3.create_bucket(Bucket=BUCKET)
+    with open('domains/current-federal.csv', 'rb') as f:
+        s3.upload_fileobj(f, BUCKET, OBJ_KEY)
+    
+    sqs.create_queue(QueueName=SQS_URL)
+
+    r = handler.main({}, None)
+    assert r == 200
+
+
+def test_grouper():
+    from lambdas.current_domain_gatherer.handler import grouper
+    result = list(grouper(2, range(10)))
+    expected = [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9)]
+    assert result == expected
+
+
+def test_send_sqs_messages(sqs):
+    from lambdas.current_domain_gatherer.handler import send_sqs_messages
+
+    SQS_URL = os.environ['SQS_URL']
+    sqs.create_queue(QueueName=SQS_URL)
+    r = send_sqs_messages(
+        [json.dumps({'foo': 'bar'}), json.dumps({'biz': 'baz'})], 
+        sqs_queue_url=SQS_URL
+    )
+    assert "Successful" in r.keys()
