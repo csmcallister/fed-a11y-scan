f88e78b46117709154de37a7779cfe3d27dc9ef0
Scott McAllister <mcallistercs@gmail.com>
2022-06-22 09:07:44 -0400

    CLN: nix whitespace :art:

--- tests/test_domain_gatherer.py
+++ tests/test_domain_gatherer.py
@@ -5,16 +5,13 @@ from tests.fixtures import *
 
 def test_main(sqs, s3):
     from lambdas.domain_gatherer import handler
-    
     BUCKET = os.environ['BUCKET_NAME']
     OBJ_KEY = os.environ['OBJECT_KEY']
     SQS_URL = os.environ['SQS_URL']
     s3.create_bucket(Bucket=BUCKET)
     with open('domains/domains.csv', 'rb') as f:
         s3.upload_fileobj(f, BUCKET, OBJ_KEY)
-    
     sqs.create_queue(QueueName=SQS_URL)
-
     r = handler.main({}, None)
     assert r == 200
 
@@ -28,7 +25,6 @@ def test_grouper():
 
 def test_send_sqs_messages(sqs):
     from lambdas.domain_gatherer.handler import send_sqs_messages
-
     SQS_URL = os.environ['SQS_URL']
     sqs.create_queue(QueueName=SQS_URL)
     r = send_sqs_messages(
